2e2d77c74822904f2e7fb672ff62a3785ad56d7a
Sudha8374 <49125615+Sudha8374@users.noreply.github.com>
2022-01-06 14:00:43 +0530

    Update ParserProblem.java

--- src/main/java/ParserProblem.java
+++ src/main/java/ParserProblem.java
@@ -1,4 +1,3 @@
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -6,32 +5,55 @@ import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.UnknownHostException;
-//import org.json.JSONObject;
+import org.json.JSONObject;
 public class ParserProblem {
-	public String par()throws Exception {
-		String url = "https://en.wikipedia.org/w/api.php?format=json&action=query&titles=SMALL&prop=revisions&rvprop=content";
-         URL obj = new URL(url);
-         HttpURLConnection con = (HttpURLConnection) obj.openConnection();
-         // optional default is GET
-         con.setRequestMethod("GET");
-         //add request header
-         BufferedReader in = new BufferedReader(
-                 new InputStreamReader(con.getInputStream()));
-         String inputLine;
-         StringBuffer response = new StringBuffer();
-         while ((inputLine = in.readLine()) != null) {
-             response.append(inputLine);
-         }
-         in.close();
-         
-         //print in String
-         //System.out.println(response.toString());
-         return response.toString();
-		
-	}
-
 	
+
+	public String getId(String url)
+		{
+			int i ;
+			String output ="";
+			try {
+				URL wikiApi = new URL (url);
+				HttpURLConnection req = (HttpURLConnection)wikiApi.openConnection();
+				BufferedReader in = new BufferedReader (new InputStreamReader(req.getInputStream()));
+				
+				
+				StringBuffer response  = new StringBuffer();
+				 while(( i=in.read())!=-1){  
+			          response.append((char)i);  
+			          }
+				// System.out.println(response.toString());
+				 JSONObject obj   = new JSONObject (response.toString());
+				 JSONObject obj2  = obj.getJSONObject("query").getJSONObject("pages").getJSONObject("1808130");
+				 
+				 output = String.valueOf(obj2.getInt("pageid"));
+				 
+				 JSONObject arr  = obj.getJSONObject("query").getJSONObject("pages").getJSONObject("1808130").getJSONArray("revisions").getJSONObject(0);
+				
+				 
+				 String links = arr.getString("*").substring(arr.getString("*").lastIndexOf("==See also==")+13,arr.getString("*").indexOf("==References==")-2);
+				 links=links.replace(" ","_").replace("[","").replace("]","").replace("*","");
+				 String linkOut[] = links.split("\\n");
+				 for (int j =0 ; j<linkOut.length;j++) {
+					 System.out.println( "https://en.wikipedia.org/"+ linkOut[j]);
+					 System.out.println();
+				 }
+				 
+				 in.close();
+
+			}
+			catch(MalformedURLException ex)
+			{
+				throw new IllegalArgumentException("unknown protocol: htts");
+			}
+			catch(UnknownHostException ex) {
+				throw new IllegalArgumentException("en.wikipeda.org");
+			} catch (IOException e) {
+				
+				e.printStackTrace();
+			}
+			
+			return output;
+		}
 	}
-
-		 
-
